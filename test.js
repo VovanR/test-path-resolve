86eece0e747d8e951d9d88051210d08f32458901
Vladimir Rodkin <mail@vovanr.com>
2016-10-18 22:02:04 +0300

    Update deps

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
 import test from 'ava';
-import 'babel-register';
 import resolvePath from './index';
 
 test('should resolve relative path', t => {
-	t.same(resolvePath('x.y', 'a/b'), 'a/x.y');
+	t.deepEqual(resolvePath('x.y', 'a/b'), 'a/x.y');
 });
 
 test('should resolve relative depth path', t => {
-	t.same(resolvePath('x.y', 'a/b/c'), 'a/b/x.y');
-	t.same(resolvePath('d/x.y', 'a/b/c'), 'a/b/d/x.y');
+	t.deepEqual(resolvePath('x.y', 'a/b/c'), 'a/b/x.y');
+	t.deepEqual(resolvePath('d/x.y', 'a/b/c'), 'a/b/d/x.y');
 });
 
 test('should resolve ./ path', t => {
-	t.same(resolvePath('./x.y', 'a/b'), 'a/x.y');
-	t.same(resolvePath('./d/x.y', 'a/b/c'), 'a/b/d/x.y');
+	t.deepEqual(resolvePath('./x.y', 'a/b'), 'a/x.y');
+	t.deepEqual(resolvePath('./d/x.y', 'a/b/c'), 'a/b/d/x.y');
 });
 
 test('should resolve ../ path', t => {
-	t.same(resolvePath('../d/x.y', 'a/b/c'), 'a/d/x.y');
-	t.same(resolvePath('../../d/x.y', 'a/b/c'), 'd/x.y');
+	t.deepEqual(resolvePath('../d/x.y', 'a/b/c'), 'a/d/x.y');
+	t.deepEqual(resolvePath('../../d/x.y', 'a/b/c'), 'd/x.y');
 });
